532fe601e09d6c2af6fa714d52da7dcb098ccbc1
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2022-12-02 18:46:55 -0300

    checkpoint commit 📳

--- SwUi_DragGesture/ContentView.swift
+++ SwUi_DragGesture/ContentView.swift
@@ -8,19 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var viewState = CGSize.zero
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            offsetDisplay
+
+            ball
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension ContentView {
+    var offsetDisplay: some View {
+        VStack {
+            Text(String(Double(viewState.width)))
+            
+            Text(String(Double(viewState.height)))
+        }
+        .font(.largeTitle)
+    }
+}
+
+
+extension ContentView {
+    var ball: some View {
+        RoundedRectangle(cornerRadius: 40)
+            .fill(viewState.height < 0 ? .blue : .red)
+            .frame(width: 80, height: 80)
+            .offset(x: viewState.width, y: viewState.height)
+            .gesture(
+                DragGesture().onChanged { value in
+                    viewState = value.translation
+                }
+                .onEnded { value in
+                    withAnimation(.spring()) {
+                        viewState = .zero
+                    }
+                }
+            )
     }
 }
