ca3508799a173b604ab7967f3b5e908b6edd6a2f
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2022-12-02 14:23:39 -0300

    Initial Commit

---
+++ SwUi_DragGesture/SwUi_DragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwUi_DragGestureApp.swift
+//  SwUi_DragGesture
+//
+//  Created by Marcelo Sampaio on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwUi_DragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
